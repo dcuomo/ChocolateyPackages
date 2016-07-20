4150548fc4dd3ea6ab79c0b23abbc1d59b0e6459
Dan Cuomo <dacuo@microsoft.com>
2016-07-19 23:00:35 -0400

    Adding PowerBI, VSCode, and untested VS Tools for Tools Machine

---
+++ ver-PowerBIDesktop/Example/VS2015U2_DSC_Configuration.ps1
@@ -0,0 +1,15 @@
+﻿Configuration VS2015U2 {
+    
+    Import-DSCResource -ModuleName cChoco
+
+    Node localhost {
+        cChocoPackageInstaller VS2015U2 {
+            Name   = 'VS2015U2'
+            #Source = 'Packager01'
+            Ensure = 'Present'
+        }
+    }
+}
+
+VS2015U2 -OutputPath c:\temp
+Start-DscConfiguration -Path c:\temp -Wait -Verbose -Force
